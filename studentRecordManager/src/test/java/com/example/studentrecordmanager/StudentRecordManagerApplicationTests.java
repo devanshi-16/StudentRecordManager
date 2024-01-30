a15296985308c8afff94c57e35365d688674f09a
devanshi-16 <147254366+devanshi-16@users.noreply.github.com>
2024-01-31 00:43:07 +0530

    Add files via upload

---
+++ studentRecordManager/src/test/java/com/example/studentrecordmanager/StudentRecordManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentrecordmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRecordManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
